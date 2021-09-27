859bcadad1f1e8765e516ec8a434c8eafccb3940
CedricPedric <99059069@mydavinci.nl>
2021-09-27 14:46:35 +0200

    laaste commit met opschoning boolean var

--- cluedo.py
+++ cluedo.py
@@ -18,23 +18,23 @@ HaarVraag = input('Welke kleur is uw haar?: ')
 
 AgeCheck = leeftijdVraag >= 18
 
-if acteerDiplomaVraag == True and AgeCheck == True and kleurVraag.lower() == 'geel' and ManOfVrouwVraag in ['Man', 'man', 'MAN'] and HaarVraag.lower() in ['blond', 'geel']:
+if acteerDiplomaVraag and AgeCheck and kleurVraag.lower() == 'geel' and ManOfVrouwVraag in ['Man', 'man', 'MAN'] and HaarVraag.lower() in ['blond', 'geel']:
     print('Je mag op auditie voor de rol van Kolonel van Geelen')
 
-elif acteerDiplomaVraag == True and AgeCheck == True and kleurVraag.lower() == 'wit' and ManOfVrouwVraag.lower() == 'vrouw' and HaarVraag.lower() in ['wit', 'grijs']:
+elif acteerDiplomaVraag and AgeCheck and kleurVraag.lower() == 'wit' and ManOfVrouwVraag.lower() == 'vrouw' and HaarVraag.lower() in ['wit', 'grijs']:
     print('Je mag op auditie voor de rol van Mevrouw de Wit')
 
-elif acteerDiplomaVraag == True and AgeCheck == True and kleurVraag.lower() == 'rood' and ManOfVrouwVraag.lower() == 'vrouw' and HaarVraag.lower() == 'zwart':
+elif acteerDiplomaVraag  and AgeCheck and kleurVraag.lower() == 'rood' and ManOfVrouwVraag.lower() == 'vrouw' and HaarVraag.lower() == 'zwart':
     print('Je mag op auditie voor de rol van Rosa Roodhart')
 
-elif (acteerDiplomaVraag == True and AgeCheck == True and kleurVraag.lower() == 'paars' and ManOfVrouwVraag in ['Man', 'man', 'MAN'] and SnorVraag == True
+elif (acteerDiplomaVraag and AgeCheck and kleurVraag.lower() == 'paars' and ManOfVrouwVraag in ['Man', 'man', 'MAN'] and SnorVraag
 and HaarVraag.lower() == 'grijs'):
     print('Je mag op auditie voor de rol van Professor Pimpel')
 
-elif acteerDiplomaVraag == True and AgeCheck == True and kleurVraag.lower() == 'groen' and ManOfVrouwVraag in ['Man', 'man', 'MAN']:
+elif acteerDiplomaVraag and AgeCheck and kleurVraag.lower() == 'groen' and ManOfVrouwVraag in ['Man', 'man', 'MAN']:
     print('Je mag op auditie voor de rol van Dominee Groenewoud')
 
-elif acteerDiplomaVraag == True and AgeCheck == True and kleurVraag.lower() == 'blauw' and ManOfVrouwVraag.lower() == 'vrouw' and HaarVraag.lower() == 'bruin':
+elif acteerDiplomaVraag and AgeCheck and kleurVraag.lower() == 'blauw' and ManOfVrouwVraag.lower() == 'vrouw' and HaarVraag.lower() == 'bruin':
     print('Je mag op auditie voor de rol van Mevrouw Blaauw van Draet')
 
 
